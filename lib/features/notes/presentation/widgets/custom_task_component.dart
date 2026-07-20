46536c11ac566f8d71f1d6bb5d3aaa91fc8d8298
rigley.costa <rigley.costa@grupomateus.com>
2026-07-20 19:10:01 -0300

    fix(tasks): align checkbox to task top

--- lib/features/notes/presentation/widgets/custom_task_component.dart
+++ lib/features/notes/presentation/widgets/custom_task_component.dart
@@ -294,7 +294,7 @@ class _CustomTaskComponentState extends State<CustomTaskComponent>
                   child: Align(
                     alignment: Alignment.topLeft,
                     child: Padding(
-                      padding: const EdgeInsets.only(top: 11, left: 11),
+                      padding: const EdgeInsets.only(left: 11),
                       child: AppTaskCheckbox(
                         value: _isComplete,
                         accentColor: taskColor,
